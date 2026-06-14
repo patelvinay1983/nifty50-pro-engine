3320b6b21ccdacafbe909fac0c911d5e7120f78c
patelvinay1983 <patelvinay1983@gmail.com>
2026-06-14 11:52:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
@@ -9,9 +10,36 @@ def home():
 @app.route("/api/status")
 def status():
     return jsonify({
-        "status": "running",
         "engine": "NIFTY50 PRO",
-        "version": "2.0"
+        "status": "running",
+        "version": "2.1"
+    })
+
+@app.route("/api/signal")
+def signal():
+
+    pcr = round(random.uniform(0.7, 1.4), 2)
+
+    signal = "NO TRADE"
+    confidence = 50
+
+    if pcr > 1.2:
+        signal = "BUY CALL"
+        confidence = 78
+
+    elif pcr < 0.8:
+        signal = "BUY PUT"
+        confidence = 78
+
+    support = random.randint(24500, 24900)
+    resistance = random.randint(24950, 25300)
+
+    return jsonify({
+        "pcr": pcr,
+        "signal": signal,
+        "confidence": confidence,
+        "support": support,
+        "resistance": resistance
     })
 
 if __name__ == "__main__":
